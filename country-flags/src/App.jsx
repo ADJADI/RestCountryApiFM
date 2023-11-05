fa2785e472e407c40ec424ebce501c78507cd89d
ADJADI <mradjadi.a@gmail.com>
2023-11-05 18:13:16 +0100

    Initial commit

---
+++ country-flags/src/App.jsx
@@ -0,0 +1,12 @@
+import react, { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="">
+      <section></section>
+    </div>
+  );
+}
+
+export default App;
